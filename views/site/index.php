aabbeab443d4aab1d5062aecbf55a8037be9b9a0
splaa-Acer <splaandrey@gmail.com>
2019-01-03 17:11:49 +0200

    Refreshing .gitignore

--- views/site/index.php
+++
@@ -1,5 +0,0 @@
-<?= $this->render('partials/index/about'); ?>
-<?= $this->render('partials/index/creative'); ?>
-<?= $this->render('partials/index/portfolio'); ?>
-<?= $this->render('partials/index/awards'); ?>
-<?= $this->render('partials/index/team'); ?>
